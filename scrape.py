e314440b4376090d3139514c4d8fa0d9671748e5
Madeline Caples <madelinecaples@gmail.com>
2020-08-17 14:38:11 -0400

    Extending the project to loop through all pages of 1000 top movies from imdb

--- scrape.py
+++ scrape.py
@@ -1,15 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-# import numpy as np
+import numpy as np
+from time import sleep
+from random import randint
 
 headers = {'Accept-Language': 'en-US, en;q=0.5'}
-
-url = 'https://www.imdb.com/search/title/?groups=top_1000&ref_=adv_prv'
-
-results = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(results.text, 'html.parser')
 
 titles = []
 years = []
@@ -19,41 +15,47 @@ metascores = []
 votes = []
 us_gross = []
 
-movie_div = soup.find_all('div', class_='lister-item mode-advanced')
+pages = np.arange(1, 1001, 50)
 
-# Looping over all the movies to extract data
-for container in movie_div:
-	# Scraping the titles
-	name = container.h3.a.text
-	titles.append(name)
+for page in pages: 
 
-	# Years
-	year = container.h3.find('span', class_='lister-item-year').text
-	years.append(year)
+    page = requests.get(f'https://www.imdb.com/search/title/?groups=top_1000&start={str(page)}&ref_=adv_nxt', headers=headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    movie_div = soup.find_all('div', class_='lister-item mode-advanced')
+    sleep(randint(2, 10))
 
-	# Runtime
-	runtime = container.p.find('span', class_='runtime').text
-	runtimes.append(runtime)
+    # Looping over all the movies to extract data
+    for container in movie_div:
+        # Scraping the titles
+        name = container.h3.a.text
+        titles.append(name)
 
-	# imdb Ratings
-	rating = float(container.strong.text)
-	imdb_ratings.append(rating)
+        # Years
+        year = container.h3.find('span', class_='lister-item-year').text
+        years.append(year)
 
-	# Metascore
-	# If there is a metascore, use that, if not insert placeholder
-	m_score = container.find(
-	    'span', class_='metascore').text if container.find(
-	        'span', class_='metascore') else '-'
-	metascores.append(m_score)
+        # Runtime
+        runtime = container.p.find('span', class_='runtime').text
+        runtimes.append(runtime)
 
-	# collect both nv spans
-	nv = container.find_all('span', attrs={'name': 'nv'})
-	# votes
-	vote = nv[0].text
-	votes.append(vote)
-	# grosses
-	grosses = nv[1].text if len(nv) > 1 else '-'
-	us_gross.append(grosses)
+        # imdb Ratings
+        rating = float(container.strong.text)
+        imdb_ratings.append(rating)
+
+        # Metascore
+        # If there is a metascore, use that, if not insert placeholder
+        m_score = container.find(
+            'span', class_='metascore').text if container.find('span', class_='metascore') else '-'
+        metascores.append(m_score)
+
+        # collect both nv spans
+        nv = container.find_all('span', attrs={'name': 'nv'})
+        # votes
+        vote = nv[0].text
+        votes.append(vote)
+        # grosses
+        grosses = nv[1].text if len(nv) > 1 else '-'
+        us_gross.append(grosses)
 
 # Initialize a data frame with pandas
 movies = pd.DataFrame({
